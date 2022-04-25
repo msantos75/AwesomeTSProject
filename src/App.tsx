84550aab52aee08f19d1192bef93a0d7302a322a
Márcio Schumann dos Santos <santos.contador@gmail.com>
2022-04-25 16:14:29 -0300

    2nd  commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const App: React.FC = () => {
+  return <View style = {style.App}> 
+    <Text style = {style.Title}>
+      Bem-vindo aos estudos!
+    </Text>
+  </View>    
+}
+
+export default App
+
+const style = StyleSheet.create({
+  App: {
+    flex: 1,
+    backgroundColor: "#111222",
+    justifyContent: "center",
+  },
+  Title: {
+    color: "white",
+    fontSize: 24,
+    textAlign : "center",
+  }
+})
